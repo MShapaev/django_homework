3e80f05db5ab439e5a7eb6953a46501406a7c1f9
Maksim Shapaev <shapaev.maxim@gmail.com>
2024-06-04 12:52:49 +0300

    initial commit

--- 2.1-databases/work_with_database/phones/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Phone
-
-
-# Register your models here.
-@admin.register(Phone)
-class PhoneAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'image',)
